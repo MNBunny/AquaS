72e2e8b4223c033774b04d6417fece1a723fd77c
MNBunny <165802871+MNBunny@users.noreply.github.com>
2024-11-18 16:48:39 +0800

    revert_4

--- main.js
+++ main.js
@@ -72,12 +72,12 @@ function fetchData() {
         storeDataInFirebase('temperature', temp);
     });
 
-    /*
+    
     // NPK Data Updates
     dataRefNPK.nitrogen.on('value', updateNPKChart);
     dataRefNPK.phosphorus.on('value', updateNPKChart);
     dataRefNPK.potassium.on('value', updateNPKChart);
-    */
+    
 
     // NPK Data Updates
     dataRefNPK.nitrogen.on('value', function(snapshot) {
