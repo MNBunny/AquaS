fe0ebea5245b2fe79bc0dd3c859b8b5d3a600937
MNBunny <meanbunnyforex@gmail.com>
2024-11-27 13:31:41 +0800

    Update Aqua_2.ino NOV27

--- Aqua_S/Aqua_S.ino
+++ Aqua_S/Aqua_S.ino
@@ -25,8 +25,8 @@
 #define SOIL_MOISTURE_PIN A0
 
 // WiFi and Firebase credentials
-#define WIFI_SSID "GlobeAtHome_d7d38_2.4"
-#define WIFI_PASSWORD "Jy6YEfHQ"
+#define WIFI_SSID "HUAWEI-Zvkm"
+#define WIFI_PASSWORD "jKNK4gmG"
 #define API_KEY "AIzaSyBdUTGzi9iQ3asge53BP3UfLALtBghNggQ"
 #define DATABASE_URL "https://swmscp-9078d-default-rtdb.firebaseio.com/"
 
